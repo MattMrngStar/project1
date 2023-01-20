3305bb5f4377ffc9923efa591c30ef8c935ceed8
Matt <mateo.baron@upb.edu.co>
2023-01-20 10:48:43 -0500

    add exclamation marks

--- main.c
+++ main.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 int main(){
-    printf("La vida es bella\n");
+    printf("La vida es bella!!!\n");
     return(EXIT_SUCCESS);
 }
 int main(){
-    printf("El feo es uno\n");
+    printf("El feo es uno!!!\n");
     return(EXIT_SUCCESS);
 }
 
