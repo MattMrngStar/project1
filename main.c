06cb3fe385d68588fab8c78999f443c671397fb7
Matt <mateo.baron@upb.edu.co>
2023-01-20 09:10:52 -0500

    Se hizo revert a maim.c

--- main.c
+++ main.c
@@ -3,7 +3,10 @@
 
 int main(){
     printf("La vida es bella\n");
-    printf("El feo es uno");
+    return(EXIT_SUCCESS);
+}
+int main(){
+    printf("El feo es uno\n");
     return(EXIT_SUCCESS);
 }
 
